5507e9f70421273f1732bdc3fee974887141e893
einarzb <einarzb@gmail.com>
2017-10-18 06:00:16 +0300

    fetching user profile pic

--- public/js/controllers/authController.js
+++ public/js/controllers/authController.js
@@ -1,4 +1,5 @@
 app.controller('AuthCtrl', function($scope , $rootScope, $http) {
+
   $scope.logout = function() {
     $rootScope.currentUser = null;
     delete $http.defaults.headers.common.Authorization;
