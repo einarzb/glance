eb3acf72a3bbaf39bf1b48631fbd5c19c271aa01
einarzb <einarzb@gmail.com>
2017-10-18 14:19:29 +0300

    some design

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,13 +1,11 @@
 app.controller('mainController', ['$scope', 'glanceFactory', '$state', function($scope, glanceFactory, $state) {
 $scope.cats = [];
 
-//getting looks from db
-  $scope.getCats = glanceFactory.getCats;
+$scope.getCats = glanceFactory.getCats;
 
-  $scope.getCats()
+$scope.getCats()
   .then(function(response){
     $scope.cats = response; //the items are populating the array
-    console.log($scope.cats);
   })
   .catch(function(error){
     console.log(error);
--- public/js/services/glanceFactory.js
+++ public/js/services/glanceFactory.js
@@ -1,7 +1,6 @@
 app.factory('glanceFactory', function($http) {
 
-  //function get looks populated in mongoose to the ctrler
-    function getCats(){
+  function getCats(){
       return $http({
       method: 'GET',
       url: 'http://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json&nojsoncallback=1'
