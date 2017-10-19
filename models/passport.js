4ace8132b89b3248ed812d81433489ead0825183
einarzb <einarzb@gmail.com>
2017-10-19 15:46:20 +0300

    refactor passport model - changed var to const

--- models/passport.js
+++ models/passport.js
@@ -1,7 +1,7 @@
-var passport = require('passport');
-var FacebookStrategy = require('passport-facebook').Strategy;
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-var FACEBOOK_APP_ID = '1965720260353467',
+const FACEBOOK_APP_ID = '1965720260353467',
     FACEBOOK_APP_SECRET = '169dd5efd1d5885816cc861b4e9804bf';
 
 //passport configuration here
@@ -11,8 +11,9 @@ passport.use(new FacebookStrategy({
     callbackURL: "http://localhost:3000/auth/facebook/callback",
     profileFields : ['id', 'displayName', 'photos', 'email', 'first_name' ]
   },
+
   function(accessToken, refreshToken, profile, done){
-    console.log("This is profile: " , profile);
+    console.log("current profile is: " , profile);
     return done(null, profile)
   }));
 
