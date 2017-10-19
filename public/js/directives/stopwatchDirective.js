49cd73a18176925235e270d25e248e9e427bb467
einarzb <einarzb@gmail.com>
2017-10-19 16:37:08 +0300

    added font-awesome

--- public/js/directives/stopwatchDirective.js
+++ public/js/directives/stopwatchDirective.js
@@ -4,7 +4,7 @@ app.directive("stopwatch",
 
     return {
       scope: true,
-      template: "<span class='timerUnit' ng-class='secColor'>{{hours}} : {{minutes}} : {{seconds}}</span>",
+      template: "</i><span class='timerUnit' ng-class='secColor'><span class='fa fa-clock-o'></span> {{hours}} : {{minutes}} : {{seconds}}</span>",
       link: function ($scope, $element, $attributes) {
         $scope.minutes;
         $scope.seconds;
