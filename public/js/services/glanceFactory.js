713380dbaf6f7235e81615e2ceba740ed2eb7487
einarzb <einarzb@gmail.com>
2017-10-19 08:43:41 +0300

    changed API resource for gallery page and added gallery link from second page of timer

--- public/js/services/glanceFactory.js
+++ public/js/services/glanceFactory.js
@@ -3,7 +3,7 @@ app.factory('glanceFactory', function($http) {
   function getCats(){
       return $http({
       method: 'GET',
-      url: 'http://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json&nojsoncallback=1'
+      url: 'https://shared.feedox.com/demoJson.json'
       }).then(function (response) {
         return response.data.items;
       });
